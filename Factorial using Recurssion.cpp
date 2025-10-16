2ed79eae1612106a93169fd73e61b0bcbe7dd499
padala satya sai uday <udaypadala909@gmail.com>
2025-10-16 16:48:50 +0530

    Add files via upload

---
+++ Factorial using Recurssion.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+int factorial (int n) { 
+    if (n == 1 || n == 0) return 1; 
+    return n * factorial (n - 1); 
+} 
+int main() { 
+    int n; 
+    cout << "Enter a number: "; 
+    cin >> n; 
+    int fact = factorial(n); 
+    cout << fact; return 0; 
+} 
